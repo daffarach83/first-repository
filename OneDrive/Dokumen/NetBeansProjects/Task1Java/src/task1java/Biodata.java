1459fab288f03791d3001701e34b53d8b629abd6
daffarach83 <daffarach@gmail.com>
2022-04-09 15:44:11 +0700

    Up Task 1 Java

---
+++ OneDrive/Dokumen/NetBeansProjects/Task1Java/src/task1java/Biodata.java
@@ -0,0 +1,43 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package task1java;
+
+/**
+ *
+ * @author Hp
+ */
+public class Biodata extends Task1Java{
+    public void NamaLengkap(){
+            System.out.println("NAMA LENGKAP: Daffara Chairunnisa Zhuliani");        
+    }
+    
+    public void TempatLahir(){
+        System.out.println("TEMPAT LAHIR: Jakarta");    
+    }
+    
+    public void TanggalLahir(){
+        System.out.println("TANGGAL LAHIR: 3 - Agustus - 2003");        
+    }
+    
+    public void Hobi(){
+        System.out.println("HOBI: Masak, Nonton film, Dengerin musik, Bermain game");    
+    }
+    
+    public void Zodiak(){
+        System.out.println("ZODIAK: Leo");        
+    }
+    
+    public void MakananFavorit(){
+        System.out.println("FAV FOOD: segala macam masakan yang ada ayamnya");    
+    }
+
+    public void CitaCita(){
+        System.out.println("CITA - CITA: bangun cat shelter");    
+    }
+    
+    
+    
+    
+}
