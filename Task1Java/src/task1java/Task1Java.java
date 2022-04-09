79a5de1c929177b82db1b3cd2274f7a742f7823c
daffarach83 <102886447+daffarach83@users.noreply.github.com>
2022-04-09 16:27:10 +0700

    Add files via upload

---
+++ Task1Java/src/task1java/Task1Java.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package task1java;
+
+/**
+ *
+ * @author Hp
+ */
+public class Task1Java {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+      Biodata get = new Biodata();
+        
+        get.NamaLengkap();
+        get.TempatLahir();
+        get.TanggalLahir();
+        get.Hobi();
+        get.Zodiak();
+        get.MakananFavorit();
+        get.CitaCita();
+    }
+    
+}
